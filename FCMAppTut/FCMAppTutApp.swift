b80ee321be425a3f6b4c33817c95b9cc7b25291c
Ashley Dube <48048698+ashleydee1999@users.noreply.github.com>
2022-07-15 12:09:32 +0200

    Initial Commit

---
+++ FCMAppTut/FCMAppTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCMAppTutApp.swift
+//  FCMAppTut
+//
+//  Created by Ashley Dube on 2022/07/15.
+//
+
+import SwiftUI
+
+@main
+struct FCMAppTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
